3e8cb880f259f2dbd00055cb17027112c29e9baa
Vijay Ganesh <vijay.ganesh0811@gmail.com>
2025-06-11 19:36:56 +0530

    RelationalOperators

---
+++ RelationalOperators.java
@@ -0,0 +1,25 @@
+/*
+ * Relational Operators are used to compare 2 values
+ * < (Lesser Than), > (Greater Than)
+ * >= (Lesser or Equal), <= (Greater or Equal)
+ * == (Equals To), != (Not Equals To)
+ * Output of RelationOperators is Boolean (true/false)
+ */
+
+public class RelationalOperators {
+
+    public static void main(String[] args) {
+
+        int num1 = 5;
+        int num2 = 6;
+
+        System.out.println(num1==num2);
+        System.out.println(num1>num2);
+        System.out.println(num1<num2);
+        System.out.println(num1>=num2);
+        System.out.println(num1<=num2);
+        System.out.println(num1!=num2);
+        
+    }
+    
+}

fa783cc2d5778446694a8c1be02d94e93ae8b0e7
Vijay Ganesh <vijay.ganesh0811@gmail.com>
2025-06-11 13:47:41 +0530

    Variables and DataTypes

---
+++ Variables.java
@@ -0,0 +1,17 @@
+/*
+ * Variables are used to store the data temporarily in-order to process the data for required output
+ * Strongly typed Language - Int/Double/String/Array etc
+ * This defines the storage type for the variables
+ */
+
+public class Variables {
+    public static void main(String[] args) {
+
+        int num1 = 5;
+        int num2 = 5;
+
+        int result = num1+num2;
+        System.out.println(result);
+        
+    }
+}

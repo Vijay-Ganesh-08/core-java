2cccdfd2d5d284eecaad1ea42270484f855af995
Vijay Ganesh <vijay.ganesh0811@gmail.com>
2025-06-11 20:06:04 +0530

    If Else Statement

---
+++ ConditionalStatement.java
@@ -0,0 +1,22 @@
+/*
+ * Decision making statements based on the outcome of conditions
+ * Conditional statements helps to execute the piece of code
+ * if..else helps us with conditional statements in java
+ */
+
+public class ConditionalStatement {
+
+    public static void main(String[] args) {
+        
+
+        int a = 10;
+        int b = 15;
+
+        if(a<b){
+            System.out.println("A is lesser to B");
+        } else {
+            System.out.println("A is greater to B");
+        }
+    }
+    
+}

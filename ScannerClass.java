938f9e80be97a558f66edb1cbde57c167067d49a
Vijay Ganesh <vijay.ganesh0811@gmail.com>
2025-06-14 13:02:31 +0530

    ScannerClass

---
+++ ScannerClass.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ScannerClass {
+    public static void main(String[] args) {
+        System.out.println("Enter The Number:");
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        System.out.println("Number :" + num);
+
+
+        System.out.print("Enter the Array Size:");
+        int size = scan.nextInt();
+        int[] nums = new int[size];
+        for (int i = 0;i<size;i++){
+            nums[i] = scan.nextInt();
+        }
+
+        for(int numArray : nums){
+            System.out.println("Array Element :" + numArray);
+        }
+
+        for (int i = 0;i<size;i++){
+            System.out.println("Array Element of Index " + i + " is " + nums[i]);
+        }
+        scan.close();
+    }
+}
